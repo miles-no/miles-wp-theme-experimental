4040e080e1f961ce129874cf03049af0298e804c
christian skaiaa <skaihigh666@gmail.com>
2023-05-09 11:42:12 +0200

    wp script loader now handles js modules

--- functions.php
+++ functions.php
@@ -302,6 +302,18 @@ function miles_2020_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'miles_2020_scripts' );
 
+/** Add type attribute  */
+add_filter('script_loader_tag', 'add_type_attribute' , 10, 3);
+function add_type_attribute($tag, $handle, $src) {
+    // if not your script, do nothing and return original $tag
+    if ( 'your-script-handle' !== $handle && !str_contains($src, 'miles-wc')  ) {
+        return $tag;
+    }
+    // change the script tag by adding type="module" and return it.
+    $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
+    return $tag;
+}
+
 /**
  * Gutenberg scripts
  */
