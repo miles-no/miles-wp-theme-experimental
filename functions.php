ac96465c1d374f91b5af231605aa5f20553006dd
Ronny Myhre Njaastad <ronny.njaastad@miles.no>
2023-08-18 13:03:03 +0200

    bump version in functions.php

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( '_S_VERSION', '1.0.9' );
 }
 
 if ( ! function_exists( 'miles_2020_setup' ) ) :
